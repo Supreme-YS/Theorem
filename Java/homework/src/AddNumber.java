4fb7eb977b83f1ed4c09d29890f90bb36b26ece4
Supreme-YS <dudtjr1225@gmail.com>
2021-11-30 18:57:43 +0900

    Update 10 week

--- Java/homework/src/AddNumber.java
+++
@@ -1,8 +0,0 @@
-public class AddNumber {
-    public int add(int a, int b) {
-        return a + b;
-    }
-    public int add(int a, int b, int c) {
-        return a + b + c;
-    }
-}
--- Java/homework/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        AddNumber addNumber = new AddNumber();
-
-        System.out.println(addNumber.add(1,2));
-        System.out.println(addNumber.add(1,2,3));
-    }
-}
